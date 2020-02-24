8618d0b86eb41f4a10747d149a2e42f51b85d56b
Sanjay Samant <Sanjay Samant>
2020-02-24 17:59:04 +0000

    First Commit

---
+++ src/main/java/com/stacksimplify/restservices/SpringBootBuildingBlocksApplication.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBuildingBlocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBuildingBlocksApplication.class, args);
+	}
+
+}
