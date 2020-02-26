601f9ab89323e73251f7ff247ab567ee18af20c3
Sanjay Samant <Sanjay Samant>
2020-02-26 18:16:39 +0000

    ResponseStatusException First Commit

---
+++ src/main/java/com/stacksimplify/restservices/exceptions/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.stacksimplify.restservices.exceptions;
+
+public class UserNotFoundException extends Exception {
+
+	private static final long serialVersionUID = -2765432893920453225L;
+
+	public UserNotFoundException(String arg0) {
+		super(arg0);
+	}
+
+}
