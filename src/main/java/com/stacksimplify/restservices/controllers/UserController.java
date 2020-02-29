3a3d4f422dc802796179050df169f590e4761602
Sanjay Samant <Sanjay Samant>
2020-02-29 15:23:22 +0000

    JPA - OneToMany

--- src/main/java/com/stacksimplify/restservices/controllers/UserController.java
+++ src/main/java/com/stacksimplify/restservices/controllers/UserController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -31,17 +32,18 @@ import com.stacksimplify.restservices.services.UserService;
 
 @RestController
 @Validated
+@RequestMapping("/users")
 public class UserController {
 
 	@Autowired
 	private UserService userService;
 	
-	@GetMapping("/users")
+	@GetMapping
 	public List<User> getAllUsers(){
 		return userService.getAllUsers();
 	}
 	
-	@PostMapping("/users")
+	@PostMapping
 	public ResponseEntity<Void> createUser(@Valid @RequestBody User user, ServletUriComponentsBuilder builder) {
 		try{
 			user=userService.createUser(user);
@@ -56,7 +58,7 @@ public class UserController {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage());
 		}
 	}
-	@GetMapping("/users/{id}")
+	@GetMapping("/{id}")
 	public Optional<User> getUserById(@PathVariable(name = "id") @Min(1) Long id){
 		try {
 			return userService.getUserById(id);
@@ -65,7 +67,7 @@ public class UserController {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
 		}
 	}
-	@PutMapping("/users/{id}")
+	@PutMapping("/{id}")
 	public User updateUserById(@PathVariable Long id, @RequestBody User user) {
 		try{
 			return userService.updateUserById(id, user);
@@ -73,11 +75,11 @@ public class UserController {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage());
 		}
 	}
-	@DeleteMapping("/users/{id}")
+	@DeleteMapping("/{id}")
 	public void deleteUserById(@PathVariable Long id) {
 		   userService.deleteUserById(id);
 	}
-	@GetMapping("/users/byusername/{username}")
+	@GetMapping("/byusername/{username}")
 	public User getUserByUsername(@PathVariable String username) throws UsernameNotFoundException {
 		User user = userService.getUserByUsername(username);
 		if(user==null)
---
+++ src/main/java/com/stacksimplify/restservices/exceptions/OrderNotFoundException.java
@@ -0,0 +1,11 @@
+package com.stacksimplify.restservices.exceptions;
+
+public class OrderNotFoundException extends Exception {
+
+	private static final long serialVersionUID = -2765432893920453225L;
+
+	public OrderNotFoundException(String arg0) {
+		super(arg0);
+	}
+
+}
