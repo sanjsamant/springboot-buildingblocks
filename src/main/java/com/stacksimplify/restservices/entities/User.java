8579c52dfd8973a2cc3c702790f2d938bbf61db8
Sanjay Samant <Sanjay Samant>
2020-03-03 19:46:29 +0000

    Versioning

--- src/main/java/com/stacksimplify/restservices/entities/User.java
+++ src/main/java/com/stacksimplify/restservices/entities/User.java
@@ -55,6 +55,9 @@ public class User extends ResourceSupport{
 	@JsonView(Views.Internal.class)
 	private List<Order> orders;
 	
+	@Column(name="ADDRESS")
+	private String address;
+	
 	public List<Order> getOrders() {
 		return orders;
 	}
@@ -67,14 +70,22 @@ public class User extends ResourceSupport{
 		
 	}
 
-	public User(Long id, String username, String firstname, String lastname, String email, String role, String ssn) {
-		this.userid = id;
+
+
+	public User(Long userid,
+			@NotEmpty(message = "Username is mandatory field, please provide username") String username,
+			@Size(min = 2, message = "FirstName should have atleast 2 characters") String firstname, String lastname,
+			String email, String role, String ssn, List<Order> orders, String address) {
+		super();
+		this.userid = userid;
 		this.username = username;
 		this.firstname = firstname;
 		this.lastname = lastname;
 		this.email = email;
 		this.role = role;
 		this.ssn = ssn;
+		this.orders = orders;
+		this.address = address;
 	}
 
 	public Long getUserid() {
@@ -133,12 +144,22 @@ public class User extends ResourceSupport{
 		this.ssn = ssn;
 	}
 
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 	@Override
 	public String toString() {
 		return "User [userid=" + userid + ", username=" + username + ", firstname=" + firstname + ", lastname="
-				+ lastname + ", email=" + email + ", role=" + role + ", ssn=" + ssn + ", orders=" + orders + "]";
+				+ lastname + ", email=" + email + ", role=" + role + ", ssn=" + ssn + ", orders=" + orders
+				+ ", address=" + address + "]";
 	}
 
+	
 
 	
 	
